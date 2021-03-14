86c91baba4c8dde0231ab8215941292290d98fb4
Auricale <derpykirby101@gmail.com>
2021-03-13 23:11:52 -0800

    I changed stuff
    No

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ function About(){
     return(
         <div className="About-div">
             <h1> Yes, you are correct, this website is crazy with a ton of features, developed by no other than the legendary Abe Z Riggs IV.</h1>
-            <h1>Pogchamp</h1>
+            <h1>swag</h1>
             <img src="https://cdn.betterttv.net/emote/5e9cdca974046462f7673006/3x"></img>
         </div>
     );
